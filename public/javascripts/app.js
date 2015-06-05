1395e08c168f6e353945b05a0b0bc8c8750167e5
jimlocascio <jiml.webdev@gmail.com>
2015-06-04 20:33:58 -0400

    add provider

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,17 @@
-define(['angularAMD', 'angular-route'], module);
-
-function module(angularAMD) {
+nerdsaid.namespace('nerdsaid.module');
+nerdsaid.module.start = function(angularAMD) {
   var app = angular.module("webapp", ['ngRoute']);
+  
+  var routes = {
+	'root':  angularAMD.route({templateUrl: 'templates/home.html', controller:'bodyTag'})
+	}
   
   app.config(function ($routeProvider) {
     $routeProvider
-		.when('/',angularAMD.route({
-			templateUrl: 'templates/home.html', controller:'bodyTag', controllerUrl:'nerdsaid'
-		}))
+		.when('/',routes.root)
   });
+  
   return angularAMD.bootstrap(app);
 }
+
+define(['angularAMD', 'angular-route'], nerdsaid.module.start );
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,17 +3,27 @@ require.config({
     
     // alias libraries paths.  Must set 'angular'
     paths: {
-        'angular': '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.min',
-        'angular-route': '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular-route.min',
-        'angularAMD': '//cdn.jsdelivr.net/angular.amd/0.2.0/angularAMD.min'
+        'angular': '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.min'
+        ,'angular-route': '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular-route.min'
+        ,'angularAMD': '//cdn.jsdelivr.net/angular.amd/0.2.0/angularAMD.min'
+        ,'custom-filters': 'filters'
+        ,'providers': 'providers'
+        ,'services': 'services'
+        ,'factories': 'factories'
+        ,'bodyTag':'controllers'
     },
     
     // Add angular modules that does not support AMD out of the box, put it in a shim
     shim: {
-        'angularAMD': ['angular'],
-        'angular-route': ['angular']
+        'angularAMD': ['angular']
+        ,'angular-route': ['angular']
+        ,'custom-filters': ['angular']
+        ,'providers': ['angular']
+        ,'services': ['angular']
+        ,'factories': ['angular']
+        ,'bodyTag': ['angular']
     },
     
     // kick start application
-    deps: ['app']
+    deps: ['nerdsaid','app']
 });
